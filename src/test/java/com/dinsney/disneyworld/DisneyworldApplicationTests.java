ccd8337acf4873a133fa99e45ac7982554200184
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-28 20:21:42 +0100

    Master - Commit Inicial

---
+++ src/test/java/com/dinsney/disneyworld/DisneyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinsney.disneyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
