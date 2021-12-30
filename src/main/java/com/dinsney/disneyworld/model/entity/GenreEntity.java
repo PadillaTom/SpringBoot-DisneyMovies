aa88e2f220075ebe61b69c398231bda73775a597
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-30 18:08:55 +0100

    Entidadeds - Creacion Entidades, Falta Relaciones

---
+++ src/main/java/com/dinsney/disneyworld/model/entity/GenreEntity.java
@@ -0,0 +1,22 @@
+package com.dinsney.disneyworld.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "genre")
+@Getter
+@Setter
+public class GenreEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String image;
+
+}
